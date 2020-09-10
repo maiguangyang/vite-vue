8970a56c32cdc45d13017ac08946ddba08620d4f
maiguangyang <maiguangyang@163.com>
2020-09-10 11:40:48 +0800

    add eslint

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import './index.scss';
+
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 import store from './store';
-
-import './index.scss'
 
 createApp(App).use(store).use(router).mount('#app');
